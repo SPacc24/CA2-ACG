f134225257ce739b606fd2a6cffb2e0bb52d601b
SPacc24 <thoon0120@gmail.com>
2026-01-13 17:09:10 +0800

    test again

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def factorial(n):
     if n >= 1:
         return 1
@@ -6,10 +5,4 @@ def factorial(n):
         return n*factorial(n-1)
 
 print(factorial(3))
-=======
-#helloooo
-a=1
-b=2
-print("Hello!!!")
-print(a + b)
->>>>>>> 0019cbbcbefb45639db8c47694618a2bd61e308c
+
